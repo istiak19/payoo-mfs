b1b31b2f09f0b8208e4f846f9164481fc64f0213
istiak19 <istiak15-14128@diu.edu.bd>
2024-09-18 22:56:03 +0600

    first commit

--- js/login.js
+++ js/login.js
@@ -1,6 +1,13 @@
 document.getElementById('button-login').addEventListener('click',function(event){
     event.preventDefault();
    
-    const phoneNumber=document.getElementById('phone-number')
-    console.log(phoneNumber.value)
+    const phoneNumber=document.getElementById('phone-number').value 
+    const pinNumber=document.getElementById('pin-number').value
+    if(phoneNumber==='6'&&pinNumber==='1234'){
+        console.log('login')
+        window.location.href='./home.html';
+    }
+    else{
+        alert('Wrong Pin')
+    }
 })
